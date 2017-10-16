fb133af18d1c048bce6aedf751b6be789d27154a
Stefan Suarez <ssuarez@2011.nhi.edu>
2017-10-16 12:50:17 -0700

    Submit Button and JS
    Ajax skeleton added in. Submit button also added. Button still needs to
    be renamed.

--- scripts/registration.js
+++ scripts/registration.js
@@ -1,4 +1,43 @@
 $(document).ready(function() {
+	var request;
 
+	$('#registration-form').submit(function(event){
+		//Prevent default behavior of POST
+		event.preventDefault();
+
+		//Abort any pre-existing request
+		if(request) {
+			request.abort();
+		}
+
+		var $form = $(this);
+
+		var $inputs = $form.find("input, select, button, textarea");
+
+		var serializedData = $form.serialize();
+
+		$inputs.prop("disabled", true);
+
+		request = $.ajax({
+			url: "./register.php",
+			type: "POST",
+			data: serializedData
+		});
+
+		request.done(function(response, textStatus, jqXHR){
+			//Code for success
+		});
+
+		request.fail(function(jqXHR, textStatus, errorThrown){
+			//Log error to console
+			console.error(
+				"The following error occurred: " + textStatus, errorThrown
+			);
+		});
+
+		request.always(function () {
+        	$inputs.prop("disabled", false);
+    	});
+	});
 	
 });
