7e3826f9b9f129d85bc9cc6c75f6116938b660d8
Ssuarez0 <ssuarez@2011.nhi.edu>
2017-10-26 19:35:56 -0700

    Large PHP Edit
    Attempt one to get the PHP talking to the database proper.

--- register.php
+++ register.php
@@ -4,85 +4,77 @@
 include 'info.php';
 
  if (($_SERVER['REQUEST_METHOD'] == 'POST') && (!empty($_POST['action']))):
-if (isset($_POST['fname'])) { $fname = $_POST['fname']; } else { $fname = ''; }
-if (isset($_POST['lname'])) { $lname = $_POST['lname']; } else { $lname = ''; }
-if (isset($_POST['myWarriorID'])) { $myWarriorID = $_POST['myWarriorID']; } else { $myWarriorID =''; }
-if (isset($_POST['myEmail'])) { $myEmail = $_POST['myEmail']; } else { $myEmail = ''; }
-if (isset($_POST['myComments'])) { $myComments = $_POST['myComments']; } else {$myComments =''; }
-if (isset($_POST['reference'])) { $reference = $_POST['reference']; } else { $reference = ''; }
-if (isset($_POST['requesttype'])) { $requesttype = $_POST['requesttype']; } else { $requesttype = ''; }
-if (isset($_POST['ajaxrequest'])) { $ajaxrequest = $_POST['ajaxrequest']; } else { $ajaxrequest = ''; }
-	$formerrors = false;
-	if ($fname === '' || $lname === '') :
-		$err_fname = '<div class="error">Sorry, you must input your full name</div>';
-		$formerrors = true;
-	endif; // input field empty
-	if ($myEmail === ''):
-		$err_email = '<div class="error">Please input an email</div>';
-		if ( $ajaxrequest ) { echo "<script>$('#myEmail').after('<div class=\"error\">Please input an email</div>');</script>"; }
-		$formerrors = true;
+	if (isset($_POST['myFirstName'])) { $fname = $_POST['myFirstName']; } else { $fname = ''; }
+	if (isset($_POST['myLastName'])) { $lname = $_POST['myLastName']; } else { $lname = ''; }
+	if (isset($_POST['myWarriorID'])) { $myWarriorID = $_POST['myWarriorID']; } else { $myWarriorID =''; }
+	if (isset($_POST['myEmail'])) { $myEmail = $_POST['myEmail']; } else { $myEmail = ''; }
+	
+	//Empty field checks
+	$form_msg = '';
+	if ($myWarriorID === ''):
+		$err_msg = 'You must input a Warrior ID';
+		return $err_msg; 
+		//requests should terminate immediately if someone enters something wrong
 	endif;
-	 //input field empty
+	if ($myEmail === ''):
+		$err_msg = 'Please input an email';
+		return $err_msg;
+	endif;
+	if ($fname === '' || $lname === '') :
+		$err_msg = 'Sorry, you must input your full name';
+		return $err_msg;
+	endif;
+	
+	
+	//Filters
 	if(!filter_var($myEmail, FILTER_VALIDATE_EMAIL)) 
-        $err_email = '<div class="error">Please input a valid email address</div>';
-		if ( $ajaxrequest ) { echo "<script>$('#myEmail').after('<div class=\"error\">Please input a valid email address</div>');</script>"; }
-		$formerrors = true;// invalid address
+        $err_msg = 'Please input a valid email address.';
+		return $err_msg;
     endif;
-
     if ( !(preg_match('00[0-9]{9}', $myWarriorID)) ) :
-    		$err_patternmatch = '<div class="error">Sorry, you must input a valid Warrior ID</div>';
-		$formerrors = true;
+    	$err_msg = 'Sorry, you must input a valid Warrior ID.';
+		return $err_msg;
 	endif; // WarriorID doesn't match  
 
     
 	if ( !(preg_match('/[A-Za-z]+/', $fname)) ) :
-		$err_patternmatch = '<div class="error">Error: Invalid input in First Name field</div>';
-		$formerrors = true;
+		$err_msg = 'Error: Invalid input in First Name field.';
+		return $err_msg;
 	endif; // pattern doesn't match
 
 	if ( !(preg_match('/[A-Za-z]+/', $lname)) ) :
-		$err_patternmatch = '<div class="error">Error: Invalid input in Last Name field</div>';
-		$formerrors = true;
+		$err_msg = 'Error: Invalid input in Last Name field.';
+		return $err_msg;
 	endif; // pattern doesn't match
   
+  //Only get here if all fields are correct
   $formdata = array (
     'fname' => $fname,
     'lname' => $lname,
     'myEmail' => $myEmail,
     'myWarriorID' => $myWarriorID,
-    'reference' => $reference,
-    'requesttype' => $requesttype
   );
 
-	if (!($formerrors)) :
-		include("log_formdb.php");
-		$forminfolink = mysqli_connect($host, $user, $password, $dbname);
-		$forminfoquery = "INSERT INTO form_info (
-		  fname,
-		  lname,
-		  myWarriorID,
-		  myEmail
-		) 
-		VALUES (
-		  '".$fname."',
-		  '".$lname."',
-		  '".$myEmail."',
-		  '".$myWarriorID."'
-		)";
-		if ($forminforesult = mysqli_query($forminfolink, $forminfoquery)):
-		  $msg = "Your form data has been processed. Thanks.";
-		  if ( $ajaxrequest ):
-		    echo "<script>$('#myform').before('<div id=\"formmessage\"><p>",$msg,"</p></div>'); $('#myform').hide();</script>";
-		  endif; // ajaxrequest
-		else:
-		  $msg = "Problem with database";
-		  if ( $ajaxrequest ):
-		    echo "<script>$('#myform').before('<div id=\"formmessage\"><p>",$msg,"</p></div>'); $('#myform').hide();</script>";
-		  endif; // ajaxrequest
-		endif; //write to database
-		mysqli_close($forminfolink);
-	endif; // check for form errors
-endif;
- 
+	include("log_formdb.php");
+	$forminfolink = mysqli_connect($host, $user, $password, $dbname);
+	$forminfoquery = "INSERT INTO form_info (
+	  fname,
+	  lname,
+	  myWarriorID,
+	  myEmail
+	) 
+	VALUES (
+	  '".$fname."',
+	  '".$lname."',
+	  '".$myEmail."',
+	  '".$myWarriorID."'
+	)";
+	if ($forminforesult = mysqli_query($forminfolink, $forminfoquery)):
+	  $form_msg = 'Thank you, ".$fname.," for registering for the hackathon!';
+	else:
+	  $form_msg = "There was an error submission. Perhaps you already registered?";
+	endif; //write to database
+	mysqli_close($forminfolink);
+	return $form_msg;
 ?>
 
