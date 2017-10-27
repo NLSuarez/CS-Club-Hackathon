f9c88ddd9dc534302a5c3478d3cba8bbbd6a459f
Stefan Suarez <ssuarez@2011.nhi.edu>
2017-10-26 21:59:31 -0700

    Update register.php

--- register.php
+++ register.php
@@ -66,7 +66,7 @@ if(!formerrors):
 	include 'info.php';
 	$host = 'localhost';
 	$dbname = 'CSStanhack';
-	$forminfolink = mysqli_connect($host, $user, $password, $dbname);
+	$forminfolink = mysqli_connect($host, $username, $password, $dbname);
 	$forminfoquery = "INSERT INTO Participants (
 	  StudentID,
 	  Email,
